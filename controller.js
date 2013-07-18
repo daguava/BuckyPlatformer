b1cc90e149742eb5e967a4db448f7a8267307990
melutzke <mitchell649@hotmail.com>
2013-07-18 01:21:17 -0500

    LOTS of work, map editing, map-generation, controller, events

--- controller.js
+++ controller.js
@@ -1,23 +1,113 @@
 var Controller = (function(){
-	
-	// constructor
-	var Controller = function(initialPriority, argType, argBlockSize){
-		this.priority(initialPriority);
-		this.elements = [];
-		this.priorityNumber;
-		this.type = argType;
-		this.blocksize = argBlockSize
-	};
-
-	Controller.prototype = {
-
-		constructor: Controller,
-
-		get: function(elementNumber) {
-			return elements[elementNumber];
+		var codes = {
+			space:		32,
+			shift:		16,
+			right:		[39, 68],
+			left:		[37, 65],
+			up:			[38, 87],
+			down:		[40, 83],
+			plus: 		189,
+			minus:		187,
+			control: 	17
 		}
 
-	};
+		var keyStates = {
+			space:		false,
+			shift:		false,
+			right:		false,
+			left:		false,
+			up:			false,
+			down:		false,
+			plus: 		false,
+			minus:		false,
+			control: 	false
+		}
 
-	return Controller;
+		var mouseStates = {
+			click: {
+				left: 	false,
+				right: 	false,
+				middle: false,
+				x: 0,
+				y: 0
+			},
+			move: {
+				x: 0,
+				y: 0
+			}
+		}
+
+
+	return {
+		poller: function(){
+
+		},
+
+		keyEventDown: function(e){
+			for(currKey in codes){
+				if(codes[currKey] instanceof Array){
+					for(var altKey = 0; altKey < codes[currKey].length; altKey++){
+						if(e.keyCode === codes[currKey][altKey]){
+							keyStates[currKey] = true;
+						}
+					}
+					
+				} else {
+					if(e.keyCode === codes[currKey]){
+						keyStates[currKey] = true;
+					}
+				}
+			}
+			
+		},
+		keyEventUp: function(e){
+			for(currKey in codes){
+				if(codes[currKey] instanceof Array){
+					for(var altKey = 0; altKey < codes[currKey].length; altKey++){
+						if(e.keyCode === codes[currKey][altKey]){
+							keyStates[currKey] = false;
+						}
+					}
+				} else {
+					if(e.keyCode === codes[currKey]){
+						keyStates[currKey] = false;
+					}
+				}
+			}
+		},
+		mouseDown: function(e){
+			switch(e.which){
+				case 1: mouseStates.click.left =   true; break;
+				case 2: mouseStates.click.middle = true; break;
+				case 3: mouseStates.click.right =  true; break;
+			}
+			mouseStates.click.x = event.offsetX?(event.offsetX):event.pageX-document.getElementById("draw_canvas").offsetLeft;
+			mouseStates.click.y = event.offsetY?(event.offsetY):event.pageY-document.getElementById("draw_canvas").offsetTop;
+		},
+		mouseUp: function(e){
+			switch(e.which){
+				case 1: mouseStates.click.left =   false; break;
+				case 2: mouseStates.click.middle = false; break;
+				case 3: mouseStates.click.right =  false; break;
+			}
+		},
+		mouseMove: function(e){
+			mouseStates.move.x = event.offsetX?(event.offsetX):event.pageX-document.getElementById("draw_canvas").offsetLeft;
+			mouseStates.move.y = event.offsetY?(event.offsetY):event.pageY-document.getElementById("draw_canvas").offsetTop;
+		},
+		key: function(btn){
+			return keyStates;
+		},
+		mouse: function(btn){
+			return mouseStates;
+		}
+
+	}
 })();
+
+
+CanvasTag.addEventListener('keydown', 	 Controller.keyEventDown,	true);
+CanvasTag.addEventListener('keyup', 	 Controller.keyEventUp,		true);
+CanvasTag.addEventListener('mouseup', 	 Controller.mouseUp, 		false);
+CanvasTag.addEventListener('mousedown', Controller.mouseDown, 		false);
+CanvasTag.addEventListener('mousemove', Controller.mouseMove,	 	false);
--- scene.js
+++ scene.js
@@ -3,190 +3,51 @@ var Scene = (function(Layer, Materials, Canvas){
 	function View(){
 
 	}
-
 	var Scene = function(map, argSize) {
-		this.blocksize = 		argSize;
-
-		this.defaultMapElement = 'Clipping';
-
-		this.view = new View();
-
-		this.Parallax = 	[];
-		this.Background = 	[];
-		this.Main = 		[];
-		this.Character = 	[];
-		this.Npc = 			[];
-		this.Foreground = 	[];
-		this.Clipping = 	[];
-
-		this.layersObject = {
-			Parallax: 		this.Parallax,
-			Background: 	this.Background,
-			Main: 			this.Main,
-			Character:  	this.Character,
-			Npc: 			this.Npc,
-			Foreground: 	this.Foreground,
-			Clipping: 		this.Clipping
-		};
-
-		this.performance = {
-			graphics: performance.now(),
-			drawCorrect: 1,
-			drawFps: 60
-		};
-
-		console.log("Logging map-file to scene conversion time...");
-		var conversionTime = performance.now();
-
-		this.layersArray = [ this.Parallax, this.Background, this.Main, this.Character, this.Npc, this.Foreground, this.Clipping ];
-
-		var mapArray; // individual array for each layer
-		var clippingArray = []; // one array for whole map
-
-		for(var currLayer = 0; currLayer < map.length; currLayer++){
-			
-			mapArray = [];
-
-			console.log("Adding new layer '" + map[currLayer].layer + "' with priority " + map[currLayer].priority);
-			var newLayer = new Layer(map[currLayer].priority, map[currLayer].layer, this.blocksize);
-
-			for(var eCount = 0; eCount < map[currLayer].elements.length; eCount++){
-
-				var tempE = map[currLayer].elements[eCount];
-				var newType, newWidth, newHeight;
-
-				newType = 	(typeof tempE.type   !== 'undefined') ? tempE.type 	  : this.defaultMapElement;
-				newWidth = 	(typeof tempE.width  !== 'undefined') ? tempE.width   : this.blocksize;
-				newHeight = (typeof tempE.height !== 'undefined') ? tempE.height  : this.blocksize;
-
-				var newTile = new Materials.createTile(newType, tempE.x, tempE.y);
-
-				if ( newTile.clips() ){
-					if( ! (clippingArray[ tempE.x ] instanceof Array) ){
-						clippingArray[ tempE.x ] = [];
-					}
-					clippingArray[ tempE.x ][ tempE.y ] = new Materials.createTile("Clipping", tempE.x, tempE.y);
-				}
-
-				if( ! (mapArray[ tempE.x ] instanceof Array) ){
-					mapArray[ tempE.x ] = [];
-				}
-
-				mapArray[ tempE.x ][ tempE.y ] = newTile;
-
-			}
-
-			for( var x = 0; x < mapArray.length; x++){
-				if( mapArray[ x ] !== undefined ){
-					for( var y = 0; y < mapArray[ x ].length; y++){
-						if( mapArray[ x ][ y ] !== undefined){
-							var mapTile = mapArray[ x ][ y ];
-							if( mapArray[ mapTile.xTile() ][ mapTile.yTile() + 1 ] !== undefined 
-								&& mapArray[ mapTile.xTile() ][ mapTile.yTile() + 1 ].type === mapTile.type ){
-								mapTile.score(8);
-							} 
-
-							if( mapArray[ mapTile.xTile() - 1 ] !== undefined 
-								&& mapArray[ mapTile.xTile() - 1 ][ mapTile.yTile() ] !== undefined 
-								&& mapArray[ mapTile.xTile() - 1 ][ mapTile.yTile() ].type === mapTile.type ){
-								mapTile.score(2);
-							} 
-
-							if( mapArray[ mapTile.xTile() ][ mapTile.yTile() - 1 ] !== undefined 
-								&& mapArray[ mapTile.xTile() ][ mapTile.yTile() - 1 ].type === mapTile.type ){
-								mapTile.score(1);
-							} 
-							
-							if( mapArray[ mapTile.xTile() + 1 ] !== undefined 
-								&& mapArray[ mapTile.xTile() + 1 ][ mapTile.yTile() ] !== undefined 
-								&& mapArray[ mapTile.xTile() + 1 ][ mapTile.yTile() ].type === mapTile.type ){
-								mapTile.score(4);
-							}
-
-							mapTile.setImage( mapTile.tileset.get( mapTile.score() ) );
-							newLayer.add( mapTile );
-						}
-					}
-
-				}
-				
-			}
-
-			this.addLayer(newLayer, map[currLayer].layer);
-
-		}
-
-		// time to optimize the clipping layer, which resides in clippingArray;
-
-		for( var x = 0; x < clippingArray.length; x++){
-			if( clippingArray[ x ] !== undefined ){
-				for( var y = 0; y < clippingArray[ x ].length; y++){
-					if( clippingArray[ x ][ y ] !== undefined){
-						var clipTile = clippingArray[ x ][ y ];
-						if( typeof clipTile.type !== 'undefined'){
-							var newX = x + Math.floor(clipTile.w() / this.blocksize);
-							if(    clippingArray[ newX ] !== undefined 
-								&& clippingArray[ newX ][ y ] !== undefined 
-								&& clippingArray[ newX ][ y ].type === clipTile.type ){
-
-								clipTile.w( clipTile.w() + clippingArray[ newX ][ y ].w() );
-								clippingArray[ newX ][ y ] = undefined;
-								y--;
-
-							}
-						}
-					}
-				}
-			}
-		}
-
-		for( var x = 0; x < clippingArray.length; x++){
-			if( clippingArray[ x ] !== undefined ){
-				for( var y = 0; y < clippingArray[ x ].length; y++){
-					if( clippingArray[ x ][ y ] !== undefined){
-						var clipTile = clippingArray[ x ][ y ];
-						if( typeof clipTile.type !== 'undefined'){
-							var newY = y + Math.floor(clipTile.h() / this.blocksize);
-							if(    clippingArray[ x ] !== undefined 
-								&& clippingArray[ x ][ newY ] !== undefined 
-								&& clippingArray[ x ][ newY ].type === clipTile.type
-								&& clippingArray[ x ][ newY ].w() === clipTile.w() ){
-
-								clipTile.h( clipTile.h() + clippingArray[ x ][ newY ].h() );
-								clippingArray[ x ][ newY ] = undefined;
-								y--;
-
-							}
-						}
-					}
-				}
-			}
-		}
-
-		var clippingLayer = new Layer(0, "Clipping", this.blocksize);
-
-		for( var x = 0; x < clippingArray.length; x++){
-			if( clippingArray[ x ] !== undefined ){
-				for( var y = 0; y < clippingArray[ x ].length; y++){
-					if( clippingArray[ x ][ y ] !== undefined){
-						var clipTile = clippingArray[ x ][ y ];
-						clippingLayer.add(clipTile);
-					}
-				}
-			}
-		}
-
-		this.addLayer(clippingLayer, "Clipping");
-
-		conversionTime = performance.now() - conversionTime;
-
-		console.log("Map generation took " + conversionTime.toFixed(3) + "ms.");
-
+		this.initialize(map, argSize);
 	};
 
 	Scene.prototype = {
 
 		constructor: Scene,
+
+		reloadMap: function(){
+			this.initialize(this.map, this.blocksize);
+		},
+		initialize: function(map, argSize){
+			this.map = 				map;
+			this.blocksize = 		argSize;
+
+			this.defaultMapElement = 'Clipping';
+
+			this.view = new View();
+
+			this.Parallax = 	[];
+			this.Background = 	[];
+			this.Main = 		[];
+			this.Character = 	[];
+			this.Npc = 			[];
+			this.Foreground = 	[];
+			this.Clipping = 	[];
+
+			this.layersObject = {
+				Parallax: 		this.Parallax,
+				Background: 	this.Background,
+				Main: 			this.Main,
+				Character:  	this.Character,
+				Npc: 			this.Npc,
+				Foreground: 	this.Foreground,
+				Clipping: 		this.Clipping
+			};
+
+			this.performance = {
+				graphics: performance.now(),
+				drawCorrect: 1,
+				drawFps: 60
+			};
+
+			this.mapGen();
+		},
 
 		addLayer: function(newLayer, destination){
 			if(this.layersObject[destination] instanceof Array){
@@ -298,9 +159,157 @@ var Scene = (function(Layer, Materials, Canvas){
 		sortLayers: function(){
 			for(var i = 0, len = this.layersArray.length; i < len; i++){
 				this.layersArray[i].sort(function(layer1, layer2){
-		   			return layer1.priorityTile() > layer2.priorityTile();
+		   			return layer1.priority() > layer2.priority();
 		   		});
 			}
+		},
+		mapGen: function(){
+			console.log("Logging map-file to scene conversion time...");
+			var conversionTime = performance.now();
+
+			this.layersArray = [ this.Parallax, this.Background, this.Main, this.Character, this.Npc, this.Foreground, this.Clipping ];
+
+			var mapArray; // individual array for each layer
+			var clippingArray = []; // one array for whole map
+
+			for(var currLayer = 0; currLayer < map.length; currLayer++){
+				
+				mapArray = [];
+
+				console.log("Adding new layer '" + map[currLayer].layer + "' with priority " + map[currLayer].priority);
+				var newLayer = new Layer(map[currLayer].priority, map[currLayer].layer, this.blocksize);
+
+				for(var eCount = 0; eCount < map[currLayer].elements.length; eCount++){
+
+					var tempE = map[currLayer].elements[eCount];
+					var newType, newWidth, newHeight;
+
+					newType = 	(typeof tempE.type   !== 'undefined') ? tempE.type 	  : this.defaultMapElement;
+					newWidth = 	(typeof tempE.width  !== 'undefined') ? tempE.width   : this.blocksize;
+					newHeight = (typeof tempE.height !== 'undefined') ? tempE.height  : this.blocksize;
+
+					var newTile = new Materials.createTile(newType, tempE.x, tempE.y);
+
+					if ( newTile.clips() ){
+						if( ! (clippingArray[ tempE.x ] instanceof Array) ){
+							clippingArray[ tempE.x ] = [];
+						}
+						clippingArray[ tempE.x ][ tempE.y ] = new Materials.createTile("Clipping", tempE.x, tempE.y);
+					}
+
+					if( ! (mapArray[ tempE.x ] instanceof Array) ){
+						mapArray[ tempE.x ] = [];
+					}
+
+					mapArray[ tempE.x ][ tempE.y ] = newTile;
+
+				}
+
+				for( var x = 0; x < mapArray.length; x++){
+					if( mapArray[ x ] !== undefined ){
+						for( var y = 0; y < mapArray[ x ].length; y++){
+							if( mapArray[ x ][ y ] !== undefined){
+								var mapTile = mapArray[ x ][ y ];
+								if( mapArray[ mapTile.xTile() ][ mapTile.yTile() + 1 ] !== undefined 
+									&& mapArray[ mapTile.xTile() ][ mapTile.yTile() + 1 ].type === mapTile.type ){
+									mapTile.score(8);
+								} 
+
+								if( mapArray[ mapTile.xTile() - 1 ] !== undefined 
+									&& mapArray[ mapTile.xTile() - 1 ][ mapTile.yTile() ] !== undefined 
+									&& mapArray[ mapTile.xTile() - 1 ][ mapTile.yTile() ].type === mapTile.type ){
+									mapTile.score(2);
+								} 
+
+								if( mapArray[ mapTile.xTile() ][ mapTile.yTile() - 1 ] !== undefined 
+									&& mapArray[ mapTile.xTile() ][ mapTile.yTile() - 1 ].type === mapTile.type ){
+									mapTile.score(1);
+								} 
+								
+								if( mapArray[ mapTile.xTile() + 1 ] !== undefined 
+									&& mapArray[ mapTile.xTile() + 1 ][ mapTile.yTile() ] !== undefined 
+									&& mapArray[ mapTile.xTile() + 1 ][ mapTile.yTile() ].type === mapTile.type ){
+									mapTile.score(4);
+								}
+
+								mapTile.setImage( mapTile.tileset.get( mapTile.score() ) );
+								newLayer.add( mapTile );
+							}
+						}
+
+					}
+					
+				}
+
+				this.addLayer(newLayer, map[currLayer].layer);
+
+			}
+
+			// time to optimize the clipping layer, which resides in clippingArray;
+
+			for( var x = 0; x < clippingArray.length; x++){
+				if( clippingArray[ x ] !== undefined ){
+					for( var y = 0; y < clippingArray[ x ].length; y++){
+						if( clippingArray[ x ][ y ] !== undefined){
+							var clipTile = clippingArray[ x ][ y ];
+							if( typeof clipTile.type !== 'undefined'){
+								var newX = x + Math.floor(clipTile.w() / this.blocksize);
+								if(    clippingArray[ newX ] !== undefined 
+									&& clippingArray[ newX ][ y ] !== undefined 
+									&& clippingArray[ newX ][ y ].type === clipTile.type ){
+
+									clipTile.w( clipTile.w() + clippingArray[ newX ][ y ].w() );
+									clippingArray[ newX ][ y ] = undefined;
+									y--;
+
+								}
+							}
+						}
+					}
+				}
+			}
+
+			for( var x = 0; x < clippingArray.length; x++){
+				if( clippingArray[ x ] !== undefined ){
+					for( var y = 0; y < clippingArray[ x ].length; y++){
+						if( clippingArray[ x ][ y ] !== undefined){
+							var clipTile = clippingArray[ x ][ y ];
+							if( typeof clipTile.type !== 'undefined'){
+								var newY = y + Math.floor(clipTile.h() / this.blocksize);
+								if(    clippingArray[ x ] !== undefined 
+									&& clippingArray[ x ][ newY ] !== undefined 
+									&& clippingArray[ x ][ newY ].type === clipTile.type
+									&& clippingArray[ x ][ newY ].w() === clipTile.w() ){
+
+									clipTile.h( clipTile.h() + clippingArray[ x ][ newY ].h() );
+									clippingArray[ x ][ newY ] = undefined;
+									y--;
+
+								}
+							}
+						}
+					}
+				}
+			}
+
+			var clippingLayer = new Layer(0, "Clipping", this.blocksize);
+
+			for( var x = 0; x < clippingArray.length; x++){
+				if( clippingArray[ x ] !== undefined ){
+					for( var y = 0; y < clippingArray[ x ].length; y++){
+						if( clippingArray[ x ][ y ] !== undefined){
+							var clipTile = clippingArray[ x ][ y ];
+							clippingLayer.add(clipTile);
+						}
+					}
+				}
+			}
+
+			this.addLayer(clippingLayer, "Clipping");
+
+			conversionTime = performance.now() - conversionTime;
+
+			console.log("Map generation took " + conversionTime.toFixed(3) + "ms.");
 		}
 
 	};
